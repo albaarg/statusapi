10acb192d5757f33a51a5e7de2489854a2bc5cc5
alba <arga.alba@gmail.com>
2022-03-28 01:40:22 -0300

    status

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Status from './component/Status';
+
+function App() {
+  return (
+    <div className="App">
+    <Status/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Status.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Status = () => {
+  return (
+    <div>
+      <p className="text-4xl font-bold p-5 m-5...">Issue Status</p>
+      <div className="bg-green-600 p-4 m-4 ml-36 mr-36 rounded w-2/3 shadow-2xl">
+        <p className="text-left text-white">All Systems Operational</p>
+      </div>
+    </div>
+  )
+}
+
+export default Status
